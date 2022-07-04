f342f0aa2469f027478c24457e033844d2833d32
Gustiviy <79268434+Gustiviy@users.noreply.github.com>
2022-07-04 18:10:26 +0300

    Add project files.

---
+++ snowie/mytime.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace snowie
+{
+    /// <summary>
+    /// Interaction logic for mytime.xaml
+    /// </summary>
+    public partial class mytime : Window
+    {
+        public mytime()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ snowie/nameEntry.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace snowie
+{
+    /// <summary>
+    /// Interaction logic for Page1.xaml
+    /// </summary>
+    public partial class nameEntry : Page
+    {
+        public nameEntry()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Application.Current.Properties["PlayerName"] = nameTextBox.Text;
+        }
+
+        private void StartGame(object sender, RoutedEventArgs e)
+        {
+            mytime mytime = new mytime();
+            mytime.Show();
+            MainWindow.GetWindow(this).Close();
+
+            
+        }
+    }
+}
