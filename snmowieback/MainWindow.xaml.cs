85b2567f6977b45885b304654c48f603a015860c
Gustiviy <79268434+Gustiviy@users.noreply.github.com>
2022-07-05 15:46:13 +0300

    audio, qol, menu

---
+++ snmowieback/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace snowie
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void textbox1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void Button_Click1(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result;
+            result = MessageBox.Show("Continue?", "Snowie Game v0.0.1", MessageBoxButton.OKCancel, MessageBoxImage.Question);
+            switch(result)
+            {
+                case MessageBoxResult.OK:
+                    nameEntry page = new nameEntry();
+                    this.Content = page;
+                    break;
+                case MessageBoxResult.Cancel:
+
+                    break;
+            }
+        }
+    }
+}
